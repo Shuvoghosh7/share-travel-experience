2272cd181dd7d1b5d9a63c359d5dce77ded33f37
Shuvoghosh7 <shuvogosh1610@gmail.com>
2024-01-09 16:17:32 +0600

    using re-useable form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,13 +1,70 @@
 "use client";
+import { Button, Col, Input, Row } from "antd";
 
+import Image from "next/image";
+import Form from "@/components/Forms/Form";
+import FormInput from "@/components/Forms/FormInput";
+import { SubmitHandler } from "react-hook-form";
+import UploadImage from "@/components/ui/UploadImage";
+
+type FormValues = {
+   id: string;
+   password: string;
+};
 
 const LoginPage = () => {
- 
-  return (
-     <div>
-        loging.....
-     </div>
-  );
+   const onSubmit: SubmitHandler<FormValues> = (data) => {
+      try {
+         console.log(data);
+      } catch (err) { }
+   };
+   return (
+      <Row
+         justify="center"
+         align="middle"
+         style={{
+            minHeight: "100vh",
+         }}
+      >
+         <Col sm={12} md={16} lg={10}>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem aut necessitatibus dolore corrupti consequatur ea dolores nemo voluptatem ipsa, cum saepe eius omnis doloribus officiis laboriosam ipsam pariatur velit accusantium?</p>
+         </Col>
+         <Col sm={12} md={8} lg={8}>
+            <h1
+               style={{
+                  margin: "15px 0px",
+               }}
+            >
+               First login your account
+            </h1>
+            <div>
+               <Form submitHandler={onSubmit}>
+                  <div>
+                     <FormInput name="id" type="text" size="large" label="User Id" />
+                  </div>
+                  <div
+                     style={{
+                        margin: "15px 0px",
+                     }}
+                  >
+                     <FormInput
+                        name="password"
+                        type="password"
+                        size="large"
+                        label="User Password"
+                     />
+                  </div>
+                  <div>
+                     <UploadImage name="files" />
+                  </div>
+                  <Button type="primary" htmlType="submit">
+                     Login
+                  </Button>
+               </Form>
+            </div>
+         </Col>
+      </Row>
+   );
 };
 
 export default LoginPage;
