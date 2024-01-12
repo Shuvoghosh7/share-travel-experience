dc4f3552ddf934ead4c5706e33abffe3927f4cde
Shuvoghosh7 <shuvogosh1610@gmail.com>
2024-01-12 16:17:19 +0600

    [Done] Add to Dynamick breadcrumb

--- src/components/ui/Contents.tsx
+++ src/components/ui/Contents.tsx
@@ -1,12 +1,36 @@
 "use client";
 import { Layout } from "antd";
 import UMBreadCrumb from "./UMBreadCrumb";
+import { useEffect, useState } from "react";
+import { usePathname, useSearchParams} from 'next/navigation'
 
 
 const { Content } = Layout;
 
 const Contents = ({ children }: { children: React.ReactNode }) => {
   const base = "admin";
+  const pathname = usePathname()
+  const[urls,setUrl]=useState<string | any>();
+  function NavigationEvents() {
+    const pathname = usePathname();
+    const searchParams = useSearchParams();
+  
+    useEffect(() => {
+      // Remove the "/admin/" prefix from the pathname
+      const cleanPathname = pathname.replace('/admin/', '');
+  
+      const queryString = searchParams.toString();
+      const url = queryString ? `${cleanPathname}?${queryString}` : cleanPathname;
+      setUrl(url)
+      console.log(url);
+      // You can now use the modified URL
+      // ...
+    }, [pathname, searchParams]);
+  
+    return null;
+  }
+
+  NavigationEvents();
 
   return (
     <Content
@@ -22,8 +46,8 @@ const Contents = ({ children }: { children: React.ReactNode }) => {
             link: `/${base}`,
           },
           {
-            label: "Admin",
-            link: `/${base}/admin`,
+            label: `${urls}`,
+            link: `${urls}`,
           },
         ]}
       />
