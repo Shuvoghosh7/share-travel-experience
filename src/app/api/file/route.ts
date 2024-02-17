db4dda355e26988f5db48c5591e0dfe6ec18c413
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2024-02-18 01:15:03 +0600

    fix build error api

--- src/app/api/file/route.ts
+++ src/app/api/file/route.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(req: NextApiRequest | NextRequest, res: NextApiResponse) {
+export async function POST(req: NextRequest | Request, res: NextApiResponse) {
   return NextResponse.json({ name: "File uploaded" });
 }
